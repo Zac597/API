d7c617898c5becca52ffe0b72e422e1dafabb68e
Isaac <isaaclima597@gmail.com>
2023-07-04 15:53:47 -0300

    inicio back-end

--- src/server.js
+++ src/server.js
@@ -2,8 +2,17 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (request, response) => {
-  response.send("Hello World");
+app.get("/message/:id/:user", (request, response) => {
+  const { id, user } = request.params
+  response.send(`
+  Id da mensagem: ${id}.
+  Para o usuário: ${user}.`);
+});
+
+app.get("/users", (request, response) => {
+  const { page, limit } = request.query;
+
+  response.send(`Página: ${page}. Mostrar: ${limit}`);
 });
 
 const PORT = 3333;
