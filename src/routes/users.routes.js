899191d6cf5bbeee89255ed9c1f6484af6fc3d0e
Isaac <isaaclima597@gmail.com>
2023-07-06 15:17:58 -0300

    node.js

---
+++ src/routes/users.routes.js
@@ -0,0 +1,11 @@
+const { Router } = require("express");
+
+const usersRoutes = Router();
+
+usersRoutes.post("/", (request, response) => {
+  const {name, email, password} = request.body
+
+  response.send(`Usuário: ${name} - E-mail: ${email} - Senha: ${password}`);
+});
+
+module.exports = usersRoutes;
