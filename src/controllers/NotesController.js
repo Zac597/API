57335c7909dc34b0daa94ecbe8e9b13d03d277c2
Zac597 <isaaclima597@gmail.com>
2024-02-20 19:46:56 -0300

    feat: update routes

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -3,7 +3,7 @@ const knex = require("../database/knex")
 class NotesController{
   async create(request, response){
     const { title, description, tags, links} = request.body;
-    const { user_id } = request.params;
+    const  user_id  = request.user.id;
 
     const [note_id] =  await knex("notes").insert({
       title,
@@ -30,7 +30,7 @@ class NotesController{
   
         await knex("tags").insert(tagsInsert);
 
-        response.json()
+        return response.json()
   }
 
   async show(request, response){
@@ -56,7 +56,9 @@ class NotesController{
   }
 
   async index(request, response) {
-    const { title, user_id, tags } = request.query
+    const { title, tags } = request.query
+
+    const user_id = request.user.id
 
     let notes;
 
@@ -64,7 +66,16 @@ class NotesController{
         const filterTags = tags.split(',').map(tag => tag.trim())
 
         notes = await knex("tags")
+        .select([
+          "notes.id",
+          "notes.title",
+          "notes.user_id",
+        ])
+        .where("notes.user_id", user_id)
+        .whereLike("notes.title", `%${title}%`)
         .whereIn("name", filterTags)
+        .innerJoin("notes", "notes.id", "tags.note_id")
+        .orderBy("notes.title")
 
       }else{
 
@@ -74,7 +85,18 @@ class NotesController{
     .orderBy("title")
 
   }
-  return response.json(notes)
+
+  const userTags = await knex("tags").where({ user_id })
+  const notesWithTags = notes.map(note => {
+    const noteTags = userTags.filter(tag => tag.note_id === note.id)
+
+    return {
+      ...note,
+      tags:noteTags
+    }
+  })
+
+  return response.json(notesWithTags)
   }
 }
 module.exports = NotesController
--- src/server.js
+++ src/server.js
@@ -1,4 +1,7 @@
 require("express-async-errors");
+
+const uploadConfig = require("./configs/upload")
+
 const migrationsRun = require("./database/sqlite/migrations");
 
 const AppError = require("./utils/AppError");
@@ -11,6 +14,8 @@ migrationsRun();
 
 const app = express();
 app.use(express.json());
+
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER))
 
 app.use(routes);
 
