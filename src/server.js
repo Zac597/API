35a2adb7a606137cff18f5e85362d550d8087947
Zac597 <isaaclima597@gmail.com>
2024-02-21 17:30:19 -0300

    feat: new dependecie

--- src/server.js
+++ src/server.js
@@ -1,25 +1,17 @@
 require("express-async-errors");
-
 const uploadConfig = require("./configs/upload")
-
+const cors = require("cors")
 const migrationsRun = require("./database/sqlite/migrations");
-
 const AppError = require("./utils/AppError");
-
-const express = require("express");
-
+const express = require("express")
 const routes = require("./routes");
-
 migrationsRun();
-
 const app = express();
+
+app.use(cors())
 app.use(express.json());
-
 app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER))
-
 app.use(routes);
-
-
 app.use((error, request, response, next) => {
   if(error instanceof AppError) {
   return response.status(error.statusCode).json({
