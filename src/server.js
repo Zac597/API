da0e9dfed434b7aa52556dc763512a5aeb0c94f9
Zac597 <isaaclima597@gmail.com>
2024-04-17 19:12:00 -0300

    feat: preparing environment for deployment

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 require("express-async-errors");
+require("dotenv/config")
 const uploadConfig = require("./configs/upload")
 const cors = require("cors")
 const migrationsRun = require("./database/sqlite/migrations");
@@ -13,12 +14,12 @@ app.use(express.json());
 app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER))
 app.use(routes);
 app.use((error, request, response, next) => {
-  if(error instanceof AppError) {
-  return response.status(error.statusCode).json({
-    status: "error",
-    message: error.message
-  });
-}
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: "error",
+      message: error.message
+    });
+  }
 
   console.error(error);
 
@@ -28,5 +29,5 @@ app.use((error, request, response, next) => {
   });
 });
 
-const PORT = 3333;
+const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
