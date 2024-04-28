a516dfb464221577cadad1420ae2dfb35d61f553
Zac597 <isaaclima597@gmail.com>
2024-04-28 09:20:02 -0300

    feat: tests

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,25 +1,17 @@
 const { hash, compare } = require("bcryptjs")
 const AppError = require("../utils/AppError")
 
-const sqliteConnection = require("../database/sqlite")
+const UserRepository = require("../repositories/userRepository")
+const sqliteConnection = require("../database/sqlite");
+const UserCreateService = require("../services/userCreateService");
 
 class UsersController {
   async create(request, response) {
-    const {name, email, password} = request.body;
-  
-    const database = await sqliteConnection();
-    const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)",[email])
+    const { name, email, password } = request.body;
 
-    if(checkUserExists){
-      throw new AppError("Este e-mail já está em uso.");
-    }
-
-    const hashedPassword = await hash(password, 8);
-
-    await database.run(
-      "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
-       [name, email, hashedPassword]
-    );
+    const userRepository = new UserRepository()
+    const userCreateService = new UserCreateService(userRepository)
+    await userCreateService.execute({ name, email, password })
 
     return response.status(201).json();
   }
@@ -31,27 +23,27 @@ class UsersController {
     const database = await sqliteConnection();
     const user = await database.get("SELECT * FROM users WHERE id = (?)", [user_id]);
 
-    if(!user) {
+    if (!user) {
       throw new AppError("Usuário não encontrado");
     }
 
     const userWithUpdateEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
 
-    if(userWithUpdateEmail && userWithUpdateEmail.id !== user.id){
+    if (userWithUpdateEmail && userWithUpdateEmail.id !== user.id) {
       throw new AppError("Este e-mail já está em uso.");
     }
-    
+
     user.name = name ?? user.name;
     user.email = email ?? user.email;
 
-    if(password && !old_password){
+    if (password && !old_password) {
       throw new AppError("Você precisa informar a senha antiga para definir a nova senha")
     }
 
-    if(password && old_password){
+    if (password && old_password) {
       const checkOldPassword = await compare(old_password, user.password);
 
-      if(!checkOldPassword) {
+      if (!checkOldPassword) {
         throw new AppError("A senha antiga não confere.");
       }
 
--- src/routes/tags.routes.js
+++ src/routes/tags.routes.js
@@ -4,7 +4,6 @@ const TagsController = require("../controllers/TagsController");
 
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated")
 
-
 const tagsRoutes = Router();
 
 const tagsController = new TagsController();
